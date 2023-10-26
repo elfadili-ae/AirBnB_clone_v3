dba646dec78a6e9a2f03c1797dea8f163f63de07
zrdouane <ramirdouane@gmail.com>
2023-10-26 04:51:31 -0700

    update API

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -3,7 +3,10 @@
 define blueprint for API.
 """
 
-from flask import blueprints
-from api.v1.views.index import *
+from flask import Blueprint
 
 app_views = Blueprint("app_views", __name__, url_prefix='/api/v1')
+"""
+blueprint API for airbnb
+"""
+from api.v1.views.index import *
