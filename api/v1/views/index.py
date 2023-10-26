65fedfd514970e5407221b815c569fa2bf1ded6a
elfadili-ae <elfadili.ae@gmail.com>
2023-10-26 20:37:42 +0100

    Style

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,9 +5,11 @@ from flask import jsonify
 from api.v1.views import app_views
 from models import storage
 
+
 @app_views.route("/status", strict_slashes=False)
 def status():
     return jsonify(status="OK")
+
 
 @app_views.route("/stats", strict_slashes=False)
 def stats():
